fe5598a2c16fae84917e90e656b6e2fc757cfe9c
hlin <Zxsaqw12@&$>
2020-02-19 17:12:41 -0500

    added classroom test

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void getStudents() {
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void addStudent() {
+    }
+
+    @Test
+    public void removeStudent() {
+    }
 }
