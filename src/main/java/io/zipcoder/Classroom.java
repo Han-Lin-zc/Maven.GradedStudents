fde3108dbcd8282e2515204c6b5386076ccb84a5
hlin <Zxsaqw12@&$>
2020-02-25 20:16:04 -0500

    Working on Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -9,20 +9,28 @@ public class Classroom {
     private ArrayList<Student> students = new ArrayList<>();
 
 
-    public Classroom (int maxNumberOfStudents) { }
+    public Classroom (int maxNumberOfStudents) {
+        this.students = new ArrayList<>();
+    }
 
-    public Classroom (Student[] students) {}
+    public Classroom (Student[] students) {
+        this.students = new ArrayList<>();
+    }
 
     public Classroom () {
         Student[] students = new Student[30];
     }
 
-    public Integer getStudents () {
-        return null;
+    public ArrayList<Student> getStudents () {
+        return students;
     }
 
     public Double getAverageExamScore() {
-        return null;
+        Double result = 0.0;
+        for (Student student : students) {
+            result += student.getAverageExamScore();
+        }
+        return result / students.size();
     }
 
     public void addStudent (Student student) {
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,18 +1,35 @@
 package io.zipcoder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ClassroomTest {
+
+
+    Classroom classroom = new Classroom();
+    Student student1 = new Student("Han", "Lin", new Double[]{100.0, 90.0, 80.0});
+    Student student2 = new Student("Adam", "Bennett", new Double[]{40.0, 50.0, 90.0});
+    Student student3 = new Student("Moe", "Ay", new Double[] { 80.0, 50.0, 70.0});
+
+
+
     @Test
     public void getStudents() {
     }
 
     @Test
     public void getAverageExamScore() {
+        Double expected = (student1.getAverageExamScore() + student2.getAverageExamScore()
+                         + student3.getAverageExamScore()) / 3.0;
+        classroom.addStudent(student1);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+        Assert.assertEquals(expected, classroom.getAverageExamScore());
     }
 
     @Test
     public void addStudent() {
+
     }
 
     @Test
